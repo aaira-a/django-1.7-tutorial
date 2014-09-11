7b6573c48865bef20acbeb9ce345643826baa882
azam alias <azam.alias@iproperty.com>
2014-09-11 15:03:02 +0800

    added choice inline class for entering within question add

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from polls.models import Question
-from polls.models import Choice
+from polls.models import Choice, Question
 
 # Register your models here.
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -10,7 +14,6 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
